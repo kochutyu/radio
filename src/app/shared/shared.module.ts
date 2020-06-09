70d54eae26bccc7764eb770487d4cd544ebc013c
Yura Kochut <kochutyurawork@gmail.com>
2020-06-09 22:02:09 +0300

    add module (shared)

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ]
+})
+export class SharedModule { }
